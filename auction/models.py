6768e8d7a7ec7951972d848a183d649d8c51adb9
Muhammad <Muhammad.medhat@gmaol.com>
2022-02-18 01:14:11 +0200

    Init

---
+++ auction/models.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+
+
+
+'''
+    Create Listing: Users should be able to visit a page to create a new listing. They should be able to specify a title for the listing, a text-based description, and what the starting bid should be. Users should also optionally be able to provide a URL for an image for the listing and/or a category (e.g. Fashion, Toys, Electronics, Home, etc.).
+'''
+class Auction_Listing(models.Model):
+    title = models.CharField(max_length=256)
+    price = models.FloatField()
+    img_url = models.CharField(max_length=256)
+    description = models.CharField(max_length=1024, default='')
+    starting_bid = models.DecimalField(
+        decimal_places=2, max_digits=4, default=1.0)
+
+    def __str__(self):
+        return f"({self.id}): {self.title}"
+
+
+class Bids(models.Model):
+    pass
+
+
+class Comments(models.Model):
+    pass
+
+class User(AbstractUser):
+        watch_list = models.ManyToManyField(Auction_Listing, blank=True, related_name='watch_list' )
