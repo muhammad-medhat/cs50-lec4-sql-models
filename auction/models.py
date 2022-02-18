90b6e602e7ff4f296824c09dab57ccfefdeba167
Muhammad <Muhammad.medhat@gmaol.com>
2022-02-18 18:05:58 +0200

    adding forign key for user in listings

--- auction/models.py
+++ auction/models.py
@@ -3,7 +3,9 @@ from django.db import models
 
 
 
-
+class User(AbstractUser):
+        # watch_list = models.ManyToManyField(Auction_Listing, blank=True, related_name='watch_list' )
+        pass
 
 '''
     Create Listing: Users should be able to visit a page to create a new listing. They should be able to specify a title for the listing, a text-based description, and what the starting bid should be. Users should also optionally be able to provide a URL for an image for the listing and/or a category (e.g. Fashion, Toys, Electronics, Home, etc.).
@@ -13,6 +15,7 @@ class Auction_Listing(models.Model):
     price = models.FloatField()
     img_url = models.CharField(max_length=256)
     description = models.CharField(max_length=1024, default='')
+    user  = models.ForeignKey(User, on_delete=models.CASCADE, related_name='users',  default=1)
     starting_bid = models.DecimalField(
         decimal_places=2, max_digits=4, default=1.0)
 
@@ -27,5 +30,3 @@ class Bids(models.Model):
 class Comments(models.Model):
     pass
 
-class User(AbstractUser):
-        watch_list = models.ManyToManyField(Auction_Listing, blank=True, related_name='watch_list' )
